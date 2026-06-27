343abee4c8c5e05e889056b0cf192c5dcab34f58
Prasad Ghatti <gvnavp@gmail.com>
2026-06-27 08:21:59 -0400

    fix: webview issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
+window.name = 'Valabhi Astrology';
+document.title = 'Valabhi Astrology';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
